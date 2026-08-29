e7eff1aefdf10279a9db5092b6676de8abaddb57
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-29 20:34:32 +0530

    header and footer done

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myproject import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , views.header),
+    path('' , views.footer),
 ]
