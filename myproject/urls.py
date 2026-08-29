cc9453231bb2ff079db66e8fcac3d30f719b3488
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-29 23:24:30 +0530

    addtocart page done

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.home),
     path('products/' , include('products.urls')),
+    path('cart/' , include('addtocart.urls')),
 ]
